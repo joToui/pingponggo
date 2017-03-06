04a76cee9601283f67a7af1e12775d291abc0024
joToui <joe.git@configm.com>
2017-03-06 11:49:39 +0000

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,84 @@
+import pycurl
+import time
+import StringIO
+import json
+import random
+from random import randint
+import requests
+
+
+
+response = StringIO.StringIO()
+
+
+all = '''
+while True:
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://localhost:8081/all')
+    c.setopt(pycurl.HTTPHEADER, ['X-Postmark-Server-Token: API_TOKEN_HERE', 'Accept: application/json'])
+    c.setopt(pycurl.POST, 1)
+    data = json.dumps([{"Key": "real", "Val": "24", "File": "ini.ini", "File_type": "ini"}])
+    c.setopt(pycurl.POSTFIELDS, data)
+    print (data)
+    try:
+        c.perform()
+    except:
+        print "the server is down ... "
+    c.close()
+    try:
+        print response.getvalue()
+    except:
+        print "no server ..."
+    response.close()
+    time.sleep(5)
+    print(randint(0, 50))
+
+
+'''
+
+
+while True:
+    url = 'http://localhost:8081/change_this'
+    randy_newman = randint(0, 50)  # i find this man random
+    file_type = 'JANK'
+    if randy_newman > 25:
+        file_type = 'ini'
+    else:
+        file_type = 'XML'
+    data = json.dumps([{"Key": "real", "Val": str(randy_newman), "File": file_type + "." + file_type, "File_type": file_type}])
+#    data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
+    try:
+        response = requests.get(url, data=data)
+        time.sleep(1)
+        print (response.json())
+    except:
+        print("server is downe ... \nsleeping \n")
+        time.sleep(5)
+
+
+joe = '''
+
+
+while True:
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'http://localhost:8081/change_this')
+    c.setopt(pycurl.HTTPHEADER, ['X-Postmark-Server-Token: API_TOKEN_HERE', 'Accept: application/json'])
+    c.setopt(pycurl.POST, 1)
+    randy_newman = randint(0, 50)     # i find this man random
+    data = json.dumps([{"Key": "real", "Val": str(randy_newman), "File": "ini.ini", "File_type": "ini"}])
+    c.setopt(pycurl.POSTFIELDS, data)
+    print (data)
+    try:
+        c.perform()
+    except:
+        print "the server is down ... "
+    c.close()
+    try:
+        print response.getvalue()
+    except:
+        print "no server ..."
+    response.close()
+    time.sleep(2)
+    print(randint(0, 50))
+
+'''
---
+++ restserver.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "regexp"
+    "strings"
+    //"gopkg.in/ini.v1"
+)
+
+const _CONF_DATA = ""
+
+type KvFP struct {
+    Key       string `json:"key"`
+    Val       string `json:"val"`
+    File      string `json:"file"`
+    File_type string `json:"file_type"`
+}
+
+type StatusOk struct {
+    Status string `json:"Status"`
+}
+
+type KvFPs []KvFP
+
+func change_ini(Key string, Val string, File string) {
+    fmt.Println(Key)
+    fmt.Println(Val)
+    fmt.Println(File)
+    fmt.Println(" INI file type ..... changing is ok ")
+
+}
+func change_xml(Key string, Val string, File string) {
+    fmt.Println(Key)
+    fmt.Println(Val)
+    fmt.Println(File)
+    fmt.Println(" XML file type ..... changing is ok ")
+}
+
+func make_one_Change(Key string, Val string, File string, File_type string) {
+    fmt.Println(Key)
+    fmt.Println(Val)
+    fmt.Println(File)
+    fmt.Println(File_type)
+    switch File_type {
+    case "ini":
+        change_ini(Key, Val, File)
+    case "XML":
+        change_xml(Key, Val, File)
+    }
+
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnArticle(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "returns a specific article")
+    fmt.Println("Endpoint Hit: returnArticle")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "All Articles")
+    fmt.Println("Endpoint Hit: returnAllArticles")
+}
+
+func addArticle(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Adds an article to list of articles")
+    fmt.Println("Endpoint Hit: addArticle")
+}
+
+func delArticle(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "deletes a specific article")
+    fmt.Println("Endpoint Hit: delArticle")
+}
+
+func changeThis(rw http.ResponseWriter, req *http.Request) {
+    kvfps := KvFPs{}
+    /*
+       KvFP{Key: "user", Val: "joe", File: "ini.ini", File_type: "ini"},
+       KvFP{Key: "password", Val: "joe56", File: "ini.ini", File_type: "ini"},
+       KvFP{Key: "age", Val: "6", File: "ini.ini", File_type: "ini"},
+       KvFP{Key: "real", Val: "24", File: "ini.ini", File_type: "ini"},
+    */
+    fmt.Println("start ... ")
+    decoder := json.NewDecoder(req.Body)
+    err := decoder.Decode(&kvfps)
+    if err != nil {
+        panic("O.M.G This is not a Json Json is cool ...")
+    }
+    for _, json1_data := range kvfps {
+
+        make_one_Change(json1_data.Key, json1_data.Val, json1_data.File, json1_data.File_type)
+    }
+
+    fmt.Println(kvfps)
+    fmt.Println("hi ... ")
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    ///
+    ///
+    /// type Vertex struct {
+    ///   label string
+    /// }
+    ///
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    dicOK := StatusOk{Status: "ok"}
+    json.NewEncoder(rw).Encode(dicOK)
+    fmt.Println("hi the function is done ...  ")
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    ///
+    ///
+    /// type Vertex struct {
+    ///   label string
+    /// }
+    ///
+    //////////////////////////////////////////////////////////////////////////////////////////////
+}
+
+func returnAllinthisServer(rw http.ResponseWriter, req *http.Request) {
+
+    kvfps := KvFPs{}
+    /*
+       KvFP{Key: "user", Val: "joe", File: "ini.ini", File_type: "ini"},
+       KvFP{Key: "password", Val: "joe56", File: "ini.ini", File_type: "ini"},
+       KvFP{Key: "age", Val: "6", File: "ini.ini", File_type: "ini"},
+       KvFP{Key: "real", Val: "24", File: "ini.ini", File_type: "ini"},
+    */
+
+    decoder := json.NewDecoder(req.Body)
+    err := decoder.Decode(&kvfps)
+    if err != nil {
+        panic("O.M.G This is not a Json Json is cool ...")
+    }
+
+    file_in_string, err := ioutil.ReadFile("ini.ini")
+    if err != nil {
+        fmt.Println(err)
+    }
+    lines := strings.Split(string(file_in_string), "\n")
+    array_size := len(lines)
+    m := make(map[string]string)
+    //fmt.Println(array_size)
+    for i := array_size; i > 0; i-- {
+        r, _ := regexp.Compile(`[\s]*=[\s]*`)
+        keyval := r.Split(lines[i-1], 2)
+        m[keyval[0]] = keyval[1]
+        //fmt.Println(keyval[0])
+    }
+    for key, val := range m {
+        kvfps = append(kvfps, KvFP{Key: key, Val: val, File: "ini.ini", File_type: "ini"})
+        fmt.Println(key + "-->" + val)
+    }
+    fmt.Println("Endpoint Hit: returnAllinthisServer")
+    json.NewEncoder(rw).Encode(kvfps)
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    http.HandleFunc("/all", returnAllinthisServer)
+    http.HandleFunc("/change_this", changeThis)
+    http.HandleFunc("/single", returnArticle)
+    http.HandleFunc("/delete", delArticle)
+    http.HandleFunc("/add", addArticle)
+    log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+    handleRequests()
+}
